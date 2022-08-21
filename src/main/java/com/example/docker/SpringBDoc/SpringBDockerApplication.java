372594c507cfc12575886e95d28a87f91fb8b63d
Archana <archana.whatkar@gmail.com>
2022-08-21 19:54:51 +0800

    First Spring APP commit

---
+++ src/main/java/com/example/docker/SpringBDoc/SpringBDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker.SpringBDoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/docker/SpringBDoc/SpringBDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker.SpringBDoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
